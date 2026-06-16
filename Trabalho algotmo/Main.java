20bd36e8b85aea2e22e4681755ede18f7a0e467a
lXxsamuelxXl <samuelclarindofreire@gmail.com>
2026-06-16 07:35:40 -0300

    Código algoritmo

---
+++ Trabalho algotmo/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner; 
+ 
+public class Main { 
+   public static void main(String[] args) { 
+       Scanner sc = new Scanner(System.in); 
+       funcoes fu = new funcoes(); 
+       System.out.println("Bem vindo ao PetFlow!"); 
+        fu.home();
+       
+        
+ 
+ 
+ 
+ 
+   } 
+ 
+} 
+ 
